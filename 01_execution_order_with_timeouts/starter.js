e530490f9f0b51a6acfe0425e923a0b9aa42f217
simotae14 <startae14@gmail.com>
2019-05-11 13:21:16 +0200

    first question, starter file

---
+++ 01_execution_order_with_timeouts/starter.js
@@ -0,0 +1,15 @@
+/*
+Found the print sequence, show in a comment
+*/
+
+function mixing_a_thread() {
+    console.log('1');
+    console.log('2');
+    setTimeout(function() {
+        console.log('3')
+    }, 1);
+    console.log('4');
+}
+
+mixing_a_thread();
+
